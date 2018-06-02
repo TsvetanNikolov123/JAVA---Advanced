5335b53e371f1bee4e4ac59b3234ac70faab8edf
Tsvetan <tsdman1985@gmail.com>
2018-06-02 19:19:23 +0300

    finished all lab tasks

---
+++ Java Advanced May 2018/12 FUNCTIONAL PROGRAMMING/p01_SortEvenNumbers/SortEvenNumbers.java
@@ -0,0 +1,25 @@
+package p01_SortEvenNumbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortEvenNumbers {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> numbers = Arrays.stream(reader.readLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+
+        numbers.removeIf(n -> n % 2 != 0);
+        System.out.println(numbers.toString().substring(1, numbers.toString().length() - 1));
+
+        numbers.sort((a,b) -> a.compareTo(b));
+        System.out.println(numbers.toString().substring(1, numbers.toString().length() - 1));
+    }
+}
